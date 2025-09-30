8095b7c4227610a8bc8eaadc724b59f43f2592fa
Julia Portella <ju.portella21@gmail.com>
2025-09-30 13:00:25 -0300

    feat(importation): Creation of a service to persist json files data into the database

---
+++ app/services/importation/response_builder.rb
@@ -0,0 +1,65 @@
+module Importation
+  class ResponseBuilder
+    def initialize(imported_data, invalid_data)
+      @imported_data = imported_data
+      @invalid_data = invalid_data
+    end
+
+    def perform
+      {
+        success: format_success,
+        failed: format_failed
+      }
+    end
+
+    private
+
+    def format_success
+      @imported_data[:imported].map do |restaurant|
+        {
+          name: restaurant.name,
+          menus: restaurant.menus.map do |menu|
+            {
+              name: menu.name,
+              menu_items: menu.menu_items.map do |item|
+                {
+                  name: item.name,
+                  price: item.menus_menu_items.find_by(menu_id: menu.id).price
+                }
+              end
+            }
+          end
+        }
+      end
+    end
+
+    def format_failed
+      failed_imports = @imported_data[:failed].map do |f|
+        { restaurant: f[:restaurant], error: f[:error] }
+      end
+
+      failed_invalids = @invalid_data[:restaurants].map do |restaurant|
+        {
+          name: restaurant[:name],
+          error: "Invalid parameters",
+          errors: restaurant[:errors], # Adicionado para mostrar os erros do restaurante
+          menus: (restaurant[:menus] || []).map do |menu|
+            {
+              name: menu[:name] || "unknown",
+              errors: menu[:errors], # Adicionado para mostrar os erros do menu
+              items: ((menu[:menu_items] || menu[:dishes]) || []).map do |item|
+                {
+                  name: item[:name] || "unknown",
+                  price: item[:price],
+                  errors: item[:errors] # Adicionado para mostrar os erros específicos de cada item
+                }
+              end
+            }
+          end
+        }
+      end
+
+      failed_imports + failed_invalids
+    end
+  end
+end
---
+++ app/services/importation/restaurant_data.rb
@@ -0,0 +1,26 @@
+module Importation
+  class RestaurantData
+
+    def initialize(params)
+      @params = params
+    end
+
+    def perform
+      calls_params_validator
+      created_data = Importation::RestaurantsDataCreator.new(@validated_params[:valid_params]).perform
+      call_response_builder(created_data, @validated_params[:invalid_params])
+    end
+
+    private
+
+    attr_reader :params
+
+    def calls_params_validator
+      @validated_params = ::Validators::ImportationRequestParams.new(params).perform
+    end
+
+    def call_response_builder(valid, invalid)
+      Importation::ResponseBuilder.new(valid, invalid).perform
+    end
+  end
+end
