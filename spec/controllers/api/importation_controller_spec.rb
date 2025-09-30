b16b5349b14218889cd94a03df313235c754aace
Julia Portella <ju.portella21@gmail.com>
2025-09-30 20:00:26 -0300

    test(importation-services): include specs for all services used on importation

---
+++ spec/controllers/api/importation_controller_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+RSpec.describe Api::ImportationController, type: :controller do
+  describe "POST #import" do
+
+    context "when no data is sent" do
+      it "returns bad_request status with error message" do
+        post :import, params: {}
+
+        expect(response).to have_http_status(:bad_request)
+        expect(JSON.parse(response.body)).to eq({ "error" => "No data received" })
+      end
+    end
+
+    context "when valid data is sent" do
+      let(:valid_params) do
+        {
+          restaurants: [
+            {
+              name: "Test Restaurant",
+              menus: [
+                {
+                  name: "Lunch Menu",
+                  menu_items: [
+                    { name: "Burger", price: 10.5 }
+                  ],
+                  dishes: [
+                    { name: "Fries", price: 4.5 }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      end
+
+      it "calls Importation::RestaurantData and returns ok" do
+        fake_result = { success: ["Test Restaurant"], failed: [] }
+
+        expect_any_instance_of(Importation::RestaurantData).to receive(:perform).and_return(fake_result)
+
+        post :import, params: valid_params
+
+        expect(response).to have_http_status(:ok)
+        expect(JSON.parse(response.body)).to eq(JSON.parse(fake_result.to_json))
+      end
+    end
+
+    context "when import fails" do
+      let(:invalid_params) do
+        { restaurants: [{ name: "" }] }
+      end
+
+      it "returns unprocessable_entity" do
+        fake_result = { success: [], failed: ["Invalid data"] }
+
+        expect_any_instance_of(Importation::RestaurantData).to receive(:perform).and_return(fake_result)
+
+        post :import, params: invalid_params
+
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(JSON.parse(response.body)).to eq(JSON.parse(fake_result.to_json))
+      end
+    end
+
+    context "when an exception is raised" do
+      it "returns internal_server_error with message" do
+        allow_any_instance_of(Importation::RestaurantData)
+          .to receive(:perform)
+          .and_raise(StandardError, "Unexpected error")
+
+        post :import, params: { restaurants: [{ name: "Something" }] }
+
+        expect(response).to have_http_status(:internal_server_error)
+        expect(response.body).to eq("Unexpected error")
+      end
+    end
+
+  end
+end
---
+++ spec/services/importation/restaurant_data_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+
+RSpec.describe Importation::RestaurantData, type: :service do
+  let(:initial_params) do
+    {
+      restaurants: [
+        { name: "Poppo's Cafe" },
+        { name: "Casa del Poppo" }
+      ]
+    }
+  end
+
+  let(:valid_params) do
+    {
+      restaurants: [
+        {
+          name: "Poppo's Cafe",
+          menus: [
+            { name: "lunch", menu_items: [{ name: "Burger", price: 9.0 }] }
+          ]
+        }
+      ]
+    }
+  end
+
+  let(:invalid_params) do
+    {
+      restaurants: [
+        { name: "Casa del Poppo", menus: [{ name: "", menu_items: [] }] }
+      ]
+    }
+  end
+
+  let(:validated_params) { { valid_params: valid_params, invalid_params: invalid_params } }
+  let(:created_data) { { restaurants: [{ name: "Poppo's Cafe", id: 1 }] } }
+  let(:final_response) { { success: created_data[:restaurants], failed: invalid_params[:restaurants] } }
+
+  let(:validator_instance) { instance_double(Validators::ImportationRequestParams, perform: validated_params) }
+  let(:creator_instance)   { instance_double(Importation::RestaurantsDataCreator, perform: created_data) }
+  let(:response_builder_instance) { instance_double(Importation::ResponseBuilder, perform: final_response) }
+
+  subject { described_class.new(initial_params) }
+
+  before do
+    allow(Validators::ImportationRequestParams).to receive(:new).with(initial_params).and_return(validator_instance)
+    allow(Importation::RestaurantsDataCreator).to receive(:new).with(valid_params).and_return(creator_instance)
+    allow(Importation::ResponseBuilder).to receive(:new).with(created_data, invalid_params).and_return(response_builder_instance)
+  end
+
+  describe '#perform' do
+    it 'validates the input parameters' do
+      subject.perform
+
+      expect(Validators::ImportationRequestParams).to have_received(:new).with(initial_params)
+      expect(validator_instance).to have_received(:perform).once
+    end
+
+    it 'creates the restaurant data for valid params' do
+      subject.perform
+
+      expect(Importation::RestaurantsDataCreator).to have_received(:new).with(valid_params)
+      expect(creator_instance).to have_received(:perform).once
+    end
+
+    it 'builds the final response including failed data' do
+      response = subject.perform
+
+      expect(Importation::ResponseBuilder).to have_received(:new).with(created_data, invalid_params)
+      expect(response_builder_instance).to have_received(:perform).once
+      expect(response).to eq(final_response)
+    end
+  end
+end
